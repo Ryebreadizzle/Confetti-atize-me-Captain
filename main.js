e41a487fc9a0886a2ca9fa36c311f3ee785b58f0
Ryebreadizzle <hobanreilly@gmail.com>
2017-01-04 01:34:35 -0500

    Added goal percent checker and http request

--- main.js
+++ main.js
@@ -8,7 +8,11 @@ var sock = io.connect('https://ws.streamjar.tv/', {
 
 var serialport = require("serialport");
 
+var request = require("request");
+
 var port;
+
+var lastPercent;
 
 function getComName(cb) {
     serialport.list(function(err, ports) {
@@ -26,6 +30,20 @@ function launchConfetti() {
     }, config.motorTime * 1000);
 }
 
+function checkGoalPercent() {
+    request("https://streamjar.tv/api/v1/goals?apikey=" + config.apiKey, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body);
+            goals = JSON.parse(body);
+            goals.forEach(function(goal) {
+                if (goal.active) {
+                    console.log(goal);
+                }
+            });
+        }
+    });
+}
+
 getComName(function(portname) {
     port = new serialport(portname, function() {
 
@@ -39,6 +57,7 @@ getComName(function(portname) {
             if (donation.amount >= config.minimumDonation) {
                 launchConfetti();
             }
+            setTimeout(checkGoalPercent, 1000 * (config.motorTime + 1));
         });
 
         sock.on('subscribe', function(subscription) {
